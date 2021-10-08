23d1f264234c904fdee9f135a6126e147caa1d9a
Fabio <fabio.lanzi@unimore.it>
2021-10-08 11:19:46 +0200

    add comments

--- post_processing.py
+++ post_processing.py
@@ -1,14 +1,30 @@
+# -*- coding: utf-8 -*-
+# ---------------------
+
 import numpy as np
 import torch
 
 
-def tensor2img(x):
+def tensor2img(tensor_img):
     # type: (torch.Tensor) -> np.ndarray
-    x = x.detach().cpu().numpy()  # from tensor to array
-    x = x.transpose((1, 2, 0))  # from CHW to HWC
-    x = (x * 255).astype(np.uint8)  # from [0,1] to [0,255]
-    return x
+    """
+    :param tensor_img: tensor image with (C,H,W) order and values in range [0,1] (float)
+    :return: numpy_img -> array image with (H,W,C) order and values in range [0,255] (uint8)
+    """
+    numpy_img = tensor_img.detach().cpu().numpy()  # from tensor to array
+    numpy_img = numpy_img.transpose((1, 2, 0))  # from CHW to HWC
+    numpy_img = (numpy_img * 255).astype(np.uint8)  # from [0,1] to [0,255]
+    return numpy_img
+
+
+def __debug():
+    import cv2
+
+    tensor_img = torch.rand((3, 256, 256))
+    img = tensor2img(tensor_img)
+    cv2.imshow('debug :)', img)
+    cv2.waitKey()
 
 
 if __name__ == '__main__':
-    debug()
+    __debug()
--- pre_processing.py
+++ pre_processing.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# ---------------------
+
 from typing import Union
 
 import cv2
@@ -6,11 +9,17 @@ import torch
 import torchvision
 
 
+# type shortcut(s):
 ArrayOrTensor = Union[np.ndarray, torch.Tensor]
 
 
 def bgr2rgb(img):
     # type: (ArrayOrTensor) -> ArrayOrTensor
+    """
+    Colorspace conversion: BGR -> RGB.
+    :param img: image (tensor or array) in BGR format
+    :return: image (tensor or array) in RGB format
+    """
     return img[[2, 1, 0]]
 
 
@@ -19,11 +28,11 @@ class ReseizeThenCrop(object):
     def __init__(self, resized_h, resized_w, crop_x_min, crop_y_min, crop_side):
         # type: (int, int, int, int, int) -> None
         """
-        :param resized_h:
-        :param resized_w:
-        :param crop_x_min:
-        :param crop_y_min:
-        :param crop_side:
+        :param resized_h: image height after resizing, but before cutting
+        :param resized_w: image width after resizing, but before cutting
+        :param crop_x_min: x coordinate of the top left corner of the crop square
+        :param crop_y_min: y coordinate of the top left corner of the crop square
+        :param crop_side: side (in pixels) of the crop square
         """
         self.h = resized_h
         self.w = resized_w
@@ -35,17 +44,27 @@ class ReseizeThenCrop(object):
 
     def apply(self, img):
         # type: (ArrayOrTensor) -> ArrayOrTensor
+        """
+        :param img: image you want to resize and crop
+        :return: resized and corpped image
+        """
         if type(img) is np.ndarray:
             img = cv2.resize(img, (self.w, self.h))
             img = img[self.y_min:self.y_max, self.x_min:self.x_max, :]
-        else:
+        elif type(img) is torch.Tensor:
             img = torchvision.transforms.Resize((self.h, self.w))(img)
             img = img[:, self.y_min:self.y_max, self.x_min:self.x_max]
+        else:
+            raise TypeError(f'image type must be {np.ndarray} or {torch.Tensor}, not `{type(img)}`')
 
         return img
 
 
     def __call__(self, img):
+        """
+        :param img: image you want to resize and crop
+        :return: resized and corpped image
+        """
         return self.apply(img)
 
 
@@ -53,6 +72,13 @@ class PreProcessingTr(object):
 
     def __init__(self, resized_h, resized_w, crop_x_min, crop_y_min, crop_side):
         # type: (int, int, int, int, int) -> None
+        """
+        :param resized_h: image height after resizing, but before cutting
+        :param resized_w: image width after resizing, but before cutting
+        :param crop_x_min: x coordinate of the top left corner of the crop square
+        :param crop_y_min: y coordinate of the top left corner of the crop square
+        :param crop_side: side (in pixels) of the crop square
+        """
         self.trs = torchvision.transforms.Compose([
             torchvision.transforms.ToTensor(), bgr2rgb,
             ReseizeThenCrop(resized_h, resized_w, crop_x_min, crop_y_min, crop_side)
