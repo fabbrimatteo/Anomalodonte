d026793c95a145b3e0761a63f0619c317db02fed
FabioLanzi <fabio.lanzi@unimore.it>
2021-10-26 15:24:18 +0200

    refactoring

--- post_processing.py
+++ post_processing.py
@@ -8,15 +8,25 @@ import torch
 def tensor2img(tensor_img):
     # type: (torch.Tensor) -> np.ndarray
     """
-    :param tensor_img: tensor image with (C,H,W) order and values in range [0,1] (float)
-    :return: numpy_img -> array image with (H,W,C) order and values in range [0,255] (uint8)
+    :param tensor_img: tensor image with (C,H,W) order
+        and values in range [0,1] (float);
+        >> NOTE: shape can be (C,H,W) or (1,C,H,W)
+    :return: numpy_img -> array image with (H,W,C) order
+        and values in range [0,255] (uint8)
     """
     numpy_img = tensor_img.detach().cpu().numpy()  # from tensor to array
+
+    # remove batch dimension (if it is a singleton dimension)
     if len(numpy_img.shape) == 4 and numpy_img.shape[0] == 1:
         numpy_img = numpy_img[0]
+
+    # from (C,H,W) order to (H,W,C) order
     if len(numpy_img.shape) == 3:
-        numpy_img = numpy_img.transpose((1, 2, 0))  # from CHW to HWC
-    numpy_img = (numpy_img * 255).astype(np.uint8)  # from [0,1] to [0,255]
+        numpy_img = numpy_img.transpose((1, 2, 0))
+
+    # from float in [0,1] to uint8 in [0, 255]
+    numpy_img = (numpy_img * 255).astype(np.uint8)
+
     return numpy_img
 
 
